a2ca5f8e0405aca5abf700f976804591c58d148c
monalysa <monalysadepadua@gmail.com>
2024-07-04 16:15:29 -0300

    Condição Composta

--- Estudos/controle-fluxo/src/CaixaEletronico.java
+++ Estudos/controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 26.0;
+        double valorSolicitado = 22.0;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo Saldo: " + saldo);
+        }
 
-        System.out.println(saldo);
+        else
+            System.out.println("Saldo Insuficiente");
+
     }
 }
